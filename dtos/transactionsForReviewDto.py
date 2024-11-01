fcce13ce352edafc3c0ca6b97a5c355dd3ce612a
vidishraj <vidishraj@gmail.com>
2024-11-01 22:52:05 +0530

    refacto:Refactored conflict table. Keys and columns.

--- dtos/transactionsForReviewDto.py
+++ dtos/transactionsForReviewDto.py
@@ -3,5 +3,4 @@ from marshmallow import Schema, fields
 
 class TransactionForReviewSchema(Schema):
     user = fields.String(required=True)
-    conflictID = fields.Integer(required=True)
     conflict = fields.String(required=True, validate=lambda s: len(s) <= 700)
--- models/transactionsForReview.py
+++ models/transactionsForReview.py
@@ -1,13 +1,16 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from models.Base import Base
+from sqlalchemy import Column, String, ForeignKey, Integer, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship
-
+from models.Base import Base
 
 class TransactionForReview(Base):
     __tablename__ = 'transactionForReview'
 
-    conflictID = Column(Integer, primary_key=True, autoincrement=True)
     user = Column(String(100), ForeignKey('users.userID'), nullable=False)
-    conflict = Column(String(700), nullable=False)
+    conflict = Column(String(500), nullable=False)
+
+    # Define a composite primary key using both user and conflict
+    __table_args__ = (
+        PrimaryKeyConstraint('user', 'conflict'),
+    )
 
     user_relationship = relationship('User', back_populates='transaction_reviews')
