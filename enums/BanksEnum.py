d021897a93e8bba8e535c75bedcd36dd90bd3a16
vidishraj <vidishraj@gmail.com>
2024-11-23 19:04:40 +0530

    refacto: Fixed enums

--- enums/BanksEnum.py
+++ enums/BanksEnum.py
@@ -5,5 +5,6 @@ class BankEnums(Enum):
     Millenia_Credit = "Millenia_Credit"
     HDFC_DEBIT = "HDFC_DEBIT"
     ICICI_AMAZON_PAY = "ICICI_AMAZON_PAY"
-    YES_BANK = "YES_BANK"
+    YES_BANK_DEBIT = "YES_BANK_DEBIT"
+    YES_BANK_ACE = "YES_BANK_ACE"
     BOI = "BOI"
--- enums/PatternEnum.py
+++ enums/PatternEnum.py
@@ -5,4 +5,4 @@ class PatternEnum(Enum):
     Millenia_Credit = "subject:(Alert : Update on your HDFC Bank Credit Card)"
     HDFC_DEBIT = "You have done a UPI txn. Check details!"
     ICICI_AMAZON_PAY = "from:(credit_cards@icicibank.com) Transaction alert for your ICICI Bank Credit Card"
-    YES_BANK = "Yes Bank - Transaction Alert"
+    YES_BANK_ACE = "Yes Bank - Transaction Alert"
