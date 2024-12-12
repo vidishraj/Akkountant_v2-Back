b65fb4fe541eb05f6dfbd447fa03959afe094126
vidishraj <vidishraj@gmail.com>
2024-12-12 14:26:45 +0530

    refacto: deleted unused dtos

--- dtos/savedTagsDto.py
+++
@@ -1,7 +0,0 @@
-from marshmallow import Schema, fields
-
-
-class SavedTagsSchema(Schema):
-    details = fields.String(required=True)
-    tag = fields.Date(required=True)
-    user = fields.String(required=True)
--- dtos/transactionsForReviewDto.py
+++
@@ -1,6 +0,0 @@
-from marshmallow import Schema, fields
-
-
-class TransactionForReviewSchema(Schema):
-    user = fields.String(required=True)
-    conflict = fields.String(required=True, validate=lambda s: len(s) <= 700)
